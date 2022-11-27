9b102c1142f938b2b0cec7b78f79e6bfd9b9d823
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-11-27 14:06:36 +0300

    Create wishes_widget.dart

---
+++ lib/view/home/widgets/wishes_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meditation_app/constants/enums/google_font_enum.dart';
+import 'package:flutter_meditation_app/constants/font/font_color.dart';
+import 'package:flutter_meditation_app/constants/font/font_weight.dart';
+import 'package:flutter_meditation_app/extensions/context_extension.dart';
+import 'package:flutter_meditation_app/widgets/scroll/custom_scroll_behavior.dart';
+
+class WishesWidget extends StatelessWidget {
+  final String wish;
+  const WishesWidget({Key? key, required this.wish}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Container(
+      margin: EdgeInsets.only(bottom: context.dynamicFem(30)),
+      child: Text(
+        wish,
+        style: SafeGoogleFont(GoogleFontEnum.helveticaNeue.font,
+            fontSize: context.dynamicFFem(20),
+            fontWeight: subTitleWeight,
+            height: context.welcomeTitleHeight,
+            color: subTitleColor),
+      ),
+    );
+  }
+}
