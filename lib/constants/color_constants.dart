e35d1a950601f38ffd853177414d7c0b083582d6
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-11-27 21:34:24 +0300

    Create color_constants.dart

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const blueBox = Color(0xff8e97fd);
+const beige = Color(0xffffeccc);
+const darkBeige = Color(0xfff7e8d0);
+const lightGray=Color(0xffebeaec);
+const beigeBox=Color(0xffffc97e);
