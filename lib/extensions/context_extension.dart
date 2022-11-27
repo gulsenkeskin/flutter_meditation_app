8d7068c48f957d4954fd75477baf1d3a25a2633b
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-11-27 21:34:38 +0300

    Update context_extension.dart

--- lib/extensions/context_extension.dart
+++ lib/extensions/context_extension.dart
@@ -15,24 +15,32 @@ extension MediaQueryExtension on BuildContext {
   double get height => mediaQuery.size.height;
   double get width => mediaQuery.size.width;
 
-
-
   double get baseWidth => 414;
 
   double get fem => width / baseWidth;
   double get ffem => fem * 0.97;
 
-
-
+  double get fem10 => fem * 10;
+  double get fem15 => fem * 15;
+  double get fem20 => fem * 20;
+  double get fem25 => fem * 25;
   double get fem30 => fem * 30;
+  double get fem32 => fem * 32;
+  double get fem35 => fem * 35;
+  double get fem70 => fem * 70;
+  double get fem85 => fem * 85;
+  double get fem177 => fem * 177;
+  double get fem210 => fem * 210;
 
-  double get titleFontSize=> dynamicFFem(16);
-  double get titleHeight=> dynamicFFem(1.2575) / fem;
-  double get titleLetterSpacing=> dynamicFem(3.84);
+  double get ffem11 => ffem * 11;
+  double get ffem12 => ffem * 12;
+  double get ffem18 => ffem * 18;
 
-  double get welcomeTitleHeight=> dynamicFFem( 1.0809999875)/fem;
+  double get titleFontSize => dynamicFFem(16);
+  double get titleHeight => dynamicFFem(1.2575) / fem;
+  double get titleLetterSpacing => dynamicFem(3.84);
 
-
+  double get welcomeTitleHeight => dynamicFFem(1.0809999875) / fem;
 
   // double get lowValue => height * 0.01;
   // double get normalValue => height * 0.02;
@@ -44,7 +52,4 @@ extension MediaQueryExtension on BuildContext {
 
   double dynamicWidth(double val) => width * val;
   double dynamicHeight(double val) => height * val;
-
 }
-
-
