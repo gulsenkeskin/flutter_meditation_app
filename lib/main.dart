84026913e658b9e922baea74214c6753d49ea718
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-12-02 11:21:38 +0300

    localization-navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,53 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_meditation_app/constants/app/app_constants.dart';
+import 'package:flutter_meditation_app/constants/project_items.dart';
+import 'package:flutter_meditation_app/product/init/localization/localization_manager.dart';
+import 'package:flutter_meditation_app/product/init/navigation/navigation_route.dart';
+import 'package:flutter_meditation_app/product/init/navigation/navigation_service.dart';
+import 'package:flutter_meditation_app/product/init/notifier/provider_list.dart';
 import 'package:flutter_meditation_app/view/home/home_view.dart';
 import 'package:flutter_meditation_app/widgets/scroll/custom_scroll_behavior.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await _init();
+  runApp(
+    MultiProvider(
+      providers: [...ApplicationProvider.instance.dependItems],
+      child: EasyLocalization(
+        supportedLocales: LocalizationManager.instance.supportedLocales,
+        path: ApplicationConstants.langAssetPath,
+        startLocale: LocalizationManager.instance.enLocale,
+        child: const MyApp(),
+      ),
+    ),
+  );
+}
+
+Future<void> _init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  //await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meditation',
-      debugShowCheckedModeBanner: false,
-      scrollBehavior: CustomScrollBehavior(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:HomeView()
-    );
+        title: ProjectItems.projectName,
+        debugShowCheckedModeBanner: false,
+        scrollBehavior: CustomScrollBehavior(),
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        onGenerateRoute: NavigationRoute.instance.generateRoute,
+        navigatorKey: NavigationService.instance.navigatorKey,
+        home: const HomeView());
   }
 }
-
