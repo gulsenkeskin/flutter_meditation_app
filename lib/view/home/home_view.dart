a5b0075ad6053ec0be27bd36be4a541c63821567
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-12-02 11:22:30 +0300

    Update home_view.dart

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_meditation_app/constants/color_constants.dart';
 
 import 'package:flutter_meditation_app/extensions/context_extension.dart';
-import 'package:flutter_meditation_app/view/home/text_constants.dart';
+import 'package:flutter_meditation_app/view/home/home_constants.dart';
 import 'package:flutter_meditation_app/view/home/widgets/center_cards.dart';
 import 'package:flutter_meditation_app/view/home/widgets/logo_title.dart';
 import 'package:flutter_meditation_app/view/home/widgets/play_card.dart';
+import 'package:flutter_meditation_app/view/home/widgets/recomended_widget.dart';
 import 'package:flutter_meditation_app/view/home/widgets/welcome_widget.dart';
 import 'package:flutter_meditation_app/view/home/widgets/wishes_widget.dart';
 import 'package:flutter_meditation_app/constants/font/font_color.dart';
@@ -15,46 +16,37 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    HomeConstants? homeConstants = HomeConstants.instance!;
     return Scaffold(
-      body: SizedBox(
+      body: Container(
+        padding: EdgeInsets.only(
+            left: context.dynamicFem(20), top: context.dynamicFem(50)),
         width: double.infinity,
-        child: Container(
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                padding: EdgeInsets.only(
-                    left: context.dynamicFem(20), top: context.dynamicFem(50)),
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    LogoTitle(logoPath: logoPath, titleColor: titleColorDark),
-                    WelcomeWidget(
-                      welcomeText: welcomeText,
-                      userName: userName,
-                    ),
-                    WishesWidget(wish: wish),
-                    CenterCards(),
-                    PlayCard(
-                      playBoxColor: darkPlayBox,
-                      img: playCardImg,
-                      title: playCardTitle,
-                      subTitle: playCardSubTitle,
-                      minuteTitle: playCardMinute,
-                      playImgPath: playCardPlayImgPath,
-                      subTitleColor: subTitleColorLightGrey,
-                      titleColor: titleColorWhite,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            LogoTitle(
+                logoPath: homeConstants.logoPath, titleColor: titleColorDark),
+            WelcomeWidget(
+              welcomeText: homeConstants.welcomeText,
+              userName: homeConstants.userName,
+            ),
+            WishesWidget(wish: homeConstants.wish),
+            const CenterCards(),
+            PlayCard(
+              playBoxColor: darkPlayBox,
+              img: homeConstants.playCardImg,
+              title: homeConstants.playCardTitle,
+              subTitle: homeConstants.playCardSubTitle,
+              minuteTitle: homeConstants.playCardMinute,
+              playImgPath: homeConstants.playCardPlayImgPath,
+              subTitleColor: subTitleColorLightGrey,
+              titleColor: titleColorWhite,
+            ),
+            RecomendedWidget(
+              text: homeConstants.recomendedText,
+            )
+          ],
         ),
       ),
     );
