31de1eab4d936e38a6b47f09aa21f076ef117c00
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-12-02 11:22:28 +0300

    Update center_cards.dart

--- lib/view/home/widgets/center_cards.dart
+++ lib/view/home/widgets/center_cards.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_meditation_app/constants/color_constants.dart';
 import 'package:flutter_meditation_app/constants/font/font_color.dart';
 import 'package:flutter_meditation_app/extensions/context_extension.dart';
-import 'package:flutter_meditation_app/view/home/text_constants.dart';
+import 'package:flutter_meditation_app/view/home/home_constants.dart';
 import 'package:flutter_meditation_app/view/home/widgets/vertical_card_widget.dart';
 
 class CenterCards extends StatelessWidget {
@@ -23,30 +23,38 @@ class CenterCards extends StatelessWidget {
         ));
   }
 
-  VerticalCardWidget verticalCard1Wrapper() => const VerticalCardWidget(
-        imgPath: imgPathVC1,
-        subTitle: subTitleVC1,
-        bottomText: bottomTextVC1,
-        title: titleVC1,
-        btnText: btnTextVC1,
-        boxColor: blueBox,
-        titleColor: beige,
-        subTitleColor: darkBeige,
-        btnBgColor: lightGray,
-        btnTitleColor: titleColorDark,
-      );
+  VerticalCardWidget verticalCard1Wrapper() {
+    HomeConstants? homeConstants = HomeConstants.instance!;
+
+    return VerticalCardWidget(
+      imgPath: homeConstants.imgPathVC1,
+      subTitle: homeConstants.subTitleVC1,
+      bottomText: homeConstants.bottomTextVC1,
+      title: homeConstants.titleVC1,
+      btnText: homeConstants.btnTextVC1,
+      boxColor: blueBox,
+      titleColor: beige,
+      subTitleColor: darkBeige,
+      btnBgColor: lightGray,
+      btnTitleColor: titleColorDark,
+    );
+  }
 
   //30 min subtitlecolor
-  VerticalCardWidget verticalCard2Wrapper() => const VerticalCardWidget(
-        imgPath: imgPathVC2,
-        subTitle: subTitleVC2,
-        bottomText: bottomTextVC1,
-        title: titleVC2,
-        btnText: btnTextVC1,
-        boxColor: beigeBox,
-        titleColor: titleColorDark,
-        subTitleColor: subTitleColorBlack,
-        btnBgColor: titleColorDark,
-        btnTitleColor: titleColorWhite,
-      );
+  VerticalCardWidget verticalCard2Wrapper() {
+    HomeConstants? homeConstants = HomeConstants.instance!;
+
+    return VerticalCardWidget(
+      imgPath: homeConstants.imgPathVC2,
+      subTitle: homeConstants.subTitleVC2,
+      bottomText: homeConstants.bottomTextVC1,
+      title: homeConstants.titleVC2,
+      btnText: homeConstants.btnTextVC1,
+      boxColor: beigeBox,
+      titleColor: titleColorDark,
+      subTitleColor: subTitleColorBlack,
+      btnBgColor: titleColorDark,
+      btnTitleColor: titleColorWhite,
+    );
+  }
 }
