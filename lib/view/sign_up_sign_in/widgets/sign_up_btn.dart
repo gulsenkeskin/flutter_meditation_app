d522cdab9e92c5c92dbc0bb04ff523c5ce886247
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-12-04 12:39:58 +0300

    Update sign_up_btn.dart

--- lib/view/sign_up_sign_in/widgets/sign_up_btn.dart
+++ lib/view/sign_up_sign_in/widgets/sign_up_btn.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meditation_app/constants/color_constants.dart';
-import 'package:flutter_meditation_app/constants/font/font_color.dart';
-import 'package:flutter_meditation_app/core/components/text/locale_text_span.dart';
-import 'package:flutter_meditation_app/core/components/text/text_ffem14.dart';
-import 'package:flutter_meditation_app/extensions/context_extension.dart';
+import 'package:flutter_meditation_app/core/components/buttons/blue_button.dart';
 import 'package:flutter_meditation_app/product/init/localization/locale_keys.g.dart';
 
 class SignUpButton extends StatelessWidget {
@@ -13,18 +9,6 @@ class SignUpButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        bottom: context.fem20,
-      ),
-      width: double.infinity,
-      height: context.fem63,
-      decoration: BoxDecoration(
-          color: blueBox, borderRadius: BorderRadius.circular(context.fem38)),
-      child: const Center(
-          child: TextFfem14(
-        text: LocaleKeys.signUp,
-      )),
-    );
+    return const BlueButton(text: LocaleKeys.signUp);
   }
 }
