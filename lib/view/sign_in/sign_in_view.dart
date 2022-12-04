383c60a685b2e8dab0231ca42519c748c7c45037
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-12-04 12:39:53 +0300

    Create sign_in_view.dart

---
+++ lib/view/sign_in/sign_in_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meditation_app/extensions/context_extension.dart';
+
+class SignInView extends StatelessWidget {
+  const SignInView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white
+        ),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: context.fem40),
+              width: double.infinity,
+              height: context.fem428_24,
+              child: Stack(
+                children: [
+                  Positioned(
+                      child: Align(
+
+                  ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
